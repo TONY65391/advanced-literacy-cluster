018d5a988cccdfea9383dbdd9d8af966e7a2d311
TONY65391 <tonyanthony65391@gmail.com>
2026-04-11 12:02:16 +0100

    12:02

--- APP/static/script.js
+++ APP/static/script.js
@@ -1,4 +1,15 @@
-const menu = document.querySelector('header .menu')
+const header = document.querySelector('header')
+const menu = header.querySelector('.menu')
+window.addEventListener('load', () => scrollHeader(window.scrollY))
+window.addEventListener('scroll', () => scrollHeader(window.scrollY))
 menu.onclick = () => {
     menu.classList.toggle('open');
+}
+
+function scrollHeader(scrollY){
+    if (scrollY > 50){
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll');
+    }
 }
